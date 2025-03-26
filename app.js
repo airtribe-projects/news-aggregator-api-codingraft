19efa83112e85537915543cfc92493ddc3de4280
codingraft <gvaibhav864@gmail.com>
2025-03-26 17:59:47 +0530

    Setup

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-const express = require('express');
+const express = require("express");
+const { connectDB } = require("./src/utils/db");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('Something bad happened', err);
-    }
-    console.log(`Server is listening on ${port}`);
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-
+connectDB();
+app.listen(port, (err) => {
+  if (err) {
+    return console.log("Something bad happened", err);
+  }
+  console.log(`Server is listening on ${port}`);
+});
 
 module.exports = app;
